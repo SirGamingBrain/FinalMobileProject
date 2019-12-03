721e4898b25d6cf8fde415c63e908f5711ad42a5
OriginalNappyBoi <54566412+OriginalNappyBoi@users.noreply.github.com>
2019-12-03 05:37:43 -0500

    Working interactions

--- Elixir Mixer Mobile/Assets/Scripts/KitchTools.cs
+++ Elixir Mixer Mobile/Assets/Scripts/KitchTools.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class KitchTools
+public class KitchTools : MonoBehaviour
 {
-    string name;
+   
     float timer;
 
-    List<string> ingredients = new List<string>();
+    List<string> Grinded = new List<string>();
+    List<string> Baked = new List<string>();
+    List<string> Extracted = new List<string>();
     List<string> potions = new List<string>();
 
+    string leftItem;
+    string rightItem;
+
+   GameObject leftHand;
+   GameObject rightHand;
 
 
+    private void Start()
+    {
+       
+
+    }
+
+    private void Update() { }
+    
+    
+
+    
 }
--- Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
+++ Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -16,13 +17,30 @@ public class PlayerScript : MonoBehaviour
     public float multiplier = 2f;
 
     string tempName;
+    string toolName;
+    string ButtonName;
+    string interactName;
+
+
+
+    int[] inventory = new int[2] {0,0};
+    List<int> CauldronInventory = new List<int>();
+
+
     List<string> Left = new List<string>();
     List<string> Right = new List<string>();
+    List<string> furnace = new List <string>();
+    List<string> basket = new List<string>();
+    List<string> cutting = new List<string>();
+    List<string> cauldron = new List<string>();
 
     public Button lefthand;
     public Button righthand;
+    
 
     Text imageName;
+
+    bool interact;
 
 
     // Start is called before the first frame update
@@ -30,9 +48,7 @@ public class PlayerScript : MonoBehaviour
     {
         joystick = FindObjectOfType<Joystick>();
         rig = this.GetComponent<Rigidbody>();
-        lefthand.GetComponent<Button>().onClick.AddListener(() => LeftButton());
-        righthand.GetComponent<Button>().onClick.AddListener(() => RightButton());
-       
+        
     }
 
     // Update is called once per frame
@@ -46,81 +62,510 @@ public class PlayerScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("Interact"))
         {
             Debug.Log("You have opened the " + other.GetComponentInChildren<Transform>().transform.parent.name + "." + " Do you want to pick it up?");
             tempName = other.GetComponentInChildren<Transform>().transform.parent.name;
+            
             Debug.Log("Pick which hand you want to pick up the object with");
+            Debug.Log(lefthand.name);
+
+          
         }
 
-        if (other.name == "Furnace")
-        {
-            Debug.Log("Furnace Call");
-        }
+        //else if (other.name == "Furnace")
+        //{
+        //    interactName = other.name;
+        //    Debug.Log("Furnace Call");
+        //    toolName = other.name;
+        //    FurnaceInteractions();
 
-        if (other.name == "Basket")
-        {
-            Debug.Log("basket Call");
-        }
+        //}
 
-        if (other.name == "bucket/Trash")
-        {
-            Debug.Log("Trash Call");
-            lefthand.GetComponent<Button>().onClick.AddListener(() => drop());
-        }
+        //else if (other.name == "Basket")
+        //{
+        //    interactName = other.name;
+        //    Debug.Log("basket Call");
+        //    toolName = other.name;
+        //}
 
-        if (other.name == "Cauldron")
-        {
-            Debug.Log("Cauldron Call");
-        }
+        //else if (other.name == "bucket/Trash")
+        //{
+        //    interactName = other.name;
+        //    Debug.Log("Trash Call");
+        //    toolName = other.name;
+        //    lefthand.onClick.AddListener(Ldrop);
+        //    righthand.onClick.AddListener(Rdrop);
+        //    lefthand.onClick.RemoveListener(LeftButton);
+        //    righthand.onClick.RemoveListener(RightButton);
 
-        if (other.name == "Cutting Board")
-        {
-            Debug.Log("Cutting Board Call");
-        }
+        //}
+
+        //else if (other.name == "Cauldron")
+        //{
+        //    interactName = other.name;
+        //    Debug.Log("Cauldron Call");
+        //    toolName = other.name;
+        //}
+
+        //else if (other.name == "Cutting Board")
+        //{
+        //    interactName = other.name;
+        //    Debug.Log("Cutting Board Call");
+        //    toolName = other.name;
+        //}
+
+       
     }
 
-    void pickup()
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
-
-   public void LeftButton()
-    {
-        Debug.Log("Left Button");
-        Left.Add(tempName);
-        tempName = "";
-        Debug.Log(Left[0]);
-        if (Left.Count > 1)
+        if (other.CompareTag("Interact"))
         {
-            Debug.Log("Hand is full");
+            interactName = other.name;
+            if (other.name == "Furnace")
+            {
+
+            }
+            else if (other.name == "Basket")
+            {
+
+            }
+            else if (other.name == "Cutting Board")
+            {
+
+            }
+            else if (other.name == "Cauldron")
+            {
+
+            }
+
+            else if (other.name == "bucket/Trash")
+            {
+
+            }
+            
+            else
+            {
+                interactName = "Ingredient";
+            }
+            tempName = other.name;
+            interact = true;
+            //Debug.Log(interact);
         }
     }
-    public void RightButton()
+
+    private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Right Button");
-        Right.Add(tempName);
-        tempName = "";
-        Debug.Log(Right[0]);
-        if (Right.Count > 1)
+        if (other.CompareTag("Interact"))
         {
-            Debug.Log("Hand is full");
+            interactName = null;
+            interact = false;
         }
     }
 
-    public void drop()
+    int trash()
     {
-        //if the left button has been clicked while someting is in the left hand list, drop that item and remove it from the list
-        if (Left.Count == 1)
+        Debug.Log("its out");
+        return 0; 
+    }
+
+    //Bake
+    int FurnaceInteractions(int hand, int item)
+    {
+        int i = 0;
+        switch (item)
         {
-            Debug.Log("you can place item into ");
+            case 0:
+                i = item;
+                Debug.Log("Empty Hand");
+                break;
+            case 2:
+                i = 9;
+                break;
+            case 3:
+                i = 10;
+                Debug.Log("grabing blood");
+                break;
+            case 4:
+                i = 11;
+                break;
+            case 5:
+                i = 12;
+                break;
+            case 6:
+                i = 13;
+                break;
+            case 8:
+                i = 14;
+                break;
+           
+            default:
+                i = item;
+                Debug.Log("Invalid Item");
+                break;
+        }
+        Debug.Log("grabbed item of value" + i + " in the " + hand);
+        return i;
+    }
+
+    //Grind
+    int BasketInteractions(int hand, int item)
+    {
+        int i = 0;
+        switch (item)
+        {
+            case 0:
+                i = item;
+                Debug.Log("Empty Hand");
+                break;
+            case 2:
+                i = 15;
+                break;
+            case 3:
+                i =16;
+                Debug.Log("grabing blood");
+                break;
+            case 4:
+                i = 17;
+                break;
+            case 5:
+                i = 18;
+                break;
+            case 7:
+                i = 19;
+                break;
+            case 8:
+                i = 20;
+                break;
+            default:
+                i = item;
+                Debug.Log("Invalid Item");
+                break;
+        }
+        Debug.Log("grabbed item of value" + i + " in the " + hand);
+        return i;
+
+
+    }
+
+    //Extract
+    int CuttingBoardInterations(int hand, int item)
+    {
+        int i = 0;
+        switch (item)
+        {
+            case 0:
+                i = item;
+                Debug.Log("Empty Hand");
+                break;
+            case 1:
+                i = 21;
+                break;
+            case 4:
+                i = 22;
+                Debug.Log("grabing blood");
+                break;
+            case 6:
+                i = 23;
+                break;
+            case 8:
+                i = 24;
+                break;  
+            default:
+                i = item;
+                Debug.Log("Invalid Item");
+                break;
+        }
+        Debug.Log("grabbed item of value" + i + " in the " + hand);
+        return i;
+
+
+    }
+
+    //Potion
+    int CauldronInteractions(int hand, int item)
+    {
+        int i = 0;
+        int size = 0;
+        switch (item)
+        {
+            case 0:
+                size = CauldronInventory.Count;
+                Debug.Log(size);
+                switch (size)
+                {
+                    case 0:
+                        Debug.Log("no potion to make");
+                        break;
+                    case 1:
+                        Debug.Log("Potion crafting failed");
+                        break;
+                    case 2:
+                        if (CauldronInventory.Contains(12) && CauldronInventory.Contains(15))
+                        {
+                            Debug.Log("Health Potion");
+                            CauldronInventory.Clear();
+                            return 25;
+                        }
+
+                        else if (CauldronInventory.Contains(10) && CauldronInventory.Contains(17))
+                        {
+                            Debug.Log("Mana Potion");
+                            CauldronInventory.Clear();
+                            return 26;
+                        }
+                        else if (CauldronInventory.Contains(18) && CauldronInventory.Contains(13))
+                        {
+                            Debug.Log("Strength Potion");
+                            CauldronInventory.Clear();
+                            return 27;
+                        }
+                        else if (CauldronInventory.Contains(23) && CauldronInventory.Contains(16))
+                        {
+                            Debug.Log("Jumping Potion");
+                            CauldronInventory.Clear();
+                            return 28;
+                        }
+                        else if (CauldronInventory.Contains(12) && CauldronInventory.Contains(14))
+                        {
+                            Debug.Log("Poision Potion");
+                            CauldronInventory.Clear();
+                            return 29;
+                        }
+                        else if (CauldronInventory.Contains(24) && CauldronInventory.Contains(21))
+                        {
+                            Debug.Log("Speed Potion");
+                            CauldronInventory.Clear();
+                            return 30;
+                        }
+
+                        else
+                        {
+                            Debug.Log("Nope");
+                        }
+                        break;
+                    case 3:
+                        if (CauldronInventory.Contains(23) && CauldronInventory.Contains(12) && CauldronInventory.Contains(24))
+                        {
+                            Debug.Log("Stamina Potion");
+                            CauldronInventory.Clear();
+                            return 31;
+                        }
+                        else if (CauldronInventory.Contains(22) && CauldronInventory.Contains(11) && CauldronInventory.Contains(17))
+                        {
+                            Debug.Log("Perception Potion");
+                            CauldronInventory.Clear();
+                            return 32;
+                        }
+                       
+                        else
+                        {
+                            Debug.Log("Nope");
+                        }
+                        break;
+                    case 4:
+                        if (CauldronInventory.Contains(20) && CauldronInventory.Contains(9) && CauldronInventory.Contains(23) && CauldronInventory.Contains(19))
+                        {
+                            Debug.Log("Revive Potion");
+                            CauldronInventory.Clear();
+                            return 33;
+                        }
+                        else
+                        {
+                            Debug.Log("Nope");
+                        }
+                        break;
+                }
+                CauldronInventory.Clear();
+                Debug.Log("Cauldron is Clear");
+                break;
+            case 9:
+                i = 9;
+                break;
+            case 10:
+                i = 10;
+                Debug.Log("grabing blood");
+                break;
+            case 11:
+                i = 11;
+                break;
+            case 12:
+                i = 12;
+                break;
+            case 13:
+                i = 13;
+                break;
+            case 14:
+                i = 14;
+                break;
+            case 15:
+                i = 15;
+                break;
+            case 16:
+                i = 16;
+                break;
+            case 17:
+                i = 17;
+                break;
+            case 18:
+                i = 18;
+                break;
+            case 19:
+                i = 19;
+                break;
+            case 20:
+                i = 20;
+                break;
+            case 21:
+                i = 21;
+                break;
+            case 22:
+                i = 22;
+                break;
+            case 23:
+                i = 23;
+                break;
+            case 24:
+                i = 24;
+                break;
+
+            default:
+                
+                Debug.Log("Invalid Item");
+                for (int j = 0; j < CauldronInventory.Count; j++)
+                {
+                    j = 0;
+                }
+                break;
+        }
+        // Debug.Log("grabbed item of value" + i + " in the " + hand);
+
+        if (CauldronInventory.Count == 4)
+        {
+            Debug.Log("Cauldron is full");
+            return item;
+        }
+        else
+        {
+            CauldronInventory.Add(i);
+            return 0;
+        }
+      
+       
+    }
+
+  public void leftpressed()
+    {
+        Debug.Log("left button pressed");
+        if (interact)
+        {
+            Debug.Log(interactName);
+            switch (interactName)
+            {
+                case "Furnace":
+                    inventory[0] = FurnaceInteractions(0, inventory[0]);
+                    break;
+                case "Basket":
+                    inventory[0] = BasketInteractions(0, inventory[0]);
+                    break;
+                case "Cutting Board":
+                    inventory[0] = CuttingBoardInterations(0, inventory[0]);
+                    break;
+                case "bucket/Trash":
+                    inventory[0] = trash();
+                    break;
+                case "Cauldron":
+                    inventory[0] = CauldronInteractions(0, inventory[0]);
+                    break;
+                case "Ingredient":
+                    Debug.Log(inventory[0]);
+                    inventory[0] = grabIngredient(0, inventory[0]);
+
+                    break;
+                 default:
+                    break;
+            }
         }
     }
 
-
-    void kitchenInteractions()
+   public void rightPressed()
     {
+        Debug.Log("right button pressed");
+        if (interact)
+        {
+            Debug.Log(interactName);
+            switch (interactName)
+            {
+                case "Furnace":
+                    inventory[1] = FurnaceInteractions(1, inventory[1]);
+                    break;
+                case "Basket":
+                    inventory[1] = BasketInteractions(1, inventory[1]);
+                    break;
+                case "Cutting Board":
+                    inventory[1] = CuttingBoardInterations(1, inventory[1]);
+                    break;
+                case "bucket/Trash":
+                    inventory[1] = trash();
+                    break;
+                case "Cauldron":
+                    inventory[1] = CauldronInteractions(1, inventory[1]);
+                    break;
+                case "Ingredient":
+                    Debug.Log(inventory[1]);
+                    inventory[1] = grabIngredient(1, inventory[1]);
 
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
+    int grabIngredient(int hand, int item)
+    {
+        int i = 0;
+        if (item == 0)
+        {
+            Debug.Log(tempName);
+            //inventory[0] = 0;
+            switch (tempName)
+            {
+                case "Blackeyed Trigger":
+                    i = 1;
+                    break;
+                case "Blood Trigger":
+                    i = 2;
+                    Debug.Log("grabing blood");
+                    break;
+                case "Blooming Trigger":
+                    i =3;
+                    break;
+                case "Cyclops Trigger":
+                    i= 4;
+                    break;
+                case "Dragon Trigger":
+                    i = 5;
+                    break;
+                case "Volcanic Trigger":
+                    i = 6;
+                    break;
+                case "Magic Trigger":
+                     i = 7;
+                    break;
+                case "Undead Trigger":
+                    i = 8;
+                    break;
+                default:
+                    break;
+            }
+            Debug.Log("grabbed item of value" + i + " in the " + hand);
+            return i;
+        }
+        Debug.Log("Hand has item" + item + " in it");
+        return hand;
+       
+    }
 }
