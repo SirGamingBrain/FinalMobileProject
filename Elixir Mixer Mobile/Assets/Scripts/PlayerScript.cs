bc0812575982ae70a72b991879c96cf70304b67e
OriginalNappyBoi <54566412+OriginalNappyBoi@users.noreply.github.com>
2019-12-02 21:43:56 -0500

    minor things
    just grabbing your changes

--- Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
+++ Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
@@ -14,7 +14,11 @@ public class PlayerScript : MonoBehaviour
 
     public float moveSpeed = 5f;
     public float multiplier = 2f;
-    
+
+    string tempName;
+    List<string> LeftHand = new List<string>();
+    List<string> RightHand = new List<string>();
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +33,33 @@ public class PlayerScript : MonoBehaviour
         Vector3 moveVector = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0f, CrossPlatformInputManager.GetAxis("Vertical")) * moveSpeed;
         transform.LookAt(transform.position + moveVector);
         rig.AddForce(moveVector);
+        pickup();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Interact"))
         {
-            Debug.Log(other.name);
+            Debug.Log("You have opened the " + other.GetComponentInChildren<Transform>().transform.parent.name + "." + " Do you want to pick it up?");
+            tempName = other.GetComponentInChildren<Transform>().transform.parent.name;
+        }
+    }
+
+    void pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            LeftHand.Add(tempName);
+            Debug.Log("You have picked up that item");
+            tempName = "";
+            LeftHand.ForEach(Debug.Log);
+            Debug.Log(tempName + "string dude");
+        }
+       
+
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("You did not pick up that item");
         }
     }
 
