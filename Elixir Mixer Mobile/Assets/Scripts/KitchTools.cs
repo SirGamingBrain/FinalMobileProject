46e9261b7d57e525b6f206842a5a6e286e9f0b3d
OriginalNappyBoi <54566412+OriginalNappyBoi@users.noreply.github.com>
2019-12-02 22:59:42 -0500

    interactions

---
+++ Elixir Mixer Mobile/Assets/Scripts/KitchTools.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchTools
+{
+    string name;
+    float timer;
+
+    List<string> ingredients = new List<string>();
+    List<string> potions = new List<string>();
+
+
+
+}
--- Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
+++ Elixir Mixer Mobile/Assets/Scripts/PlayerScript.cs
@@ -16,8 +16,13 @@ public class PlayerScript : MonoBehaviour
     public float multiplier = 2f;
 
     string tempName;
-    List<string> LeftHand = new List<string>();
-    List<string> RightHand = new List<string>();
+    List<string> Left = new List<string>();
+    List<string> Right = new List<string>();
+
+    public Button lefthand;
+    public Button righthand;
+
+    Text imageName;
 
 
     // Start is called before the first frame update
@@ -25,6 +30,9 @@ public class PlayerScript : MonoBehaviour
     {
         joystick = FindObjectOfType<Joystick>();
         rig = this.GetComponent<Rigidbody>();
+        lefthand.GetComponent<Button>().onClick.AddListener(() => LeftButton());
+        righthand.GetComponent<Button>().onClick.AddListener(() => RightButton());
+       
     }
 
     // Update is called once per frame
@@ -33,7 +41,7 @@ public class PlayerScript : MonoBehaviour
         Vector3 moveVector = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0f, CrossPlatformInputManager.GetAxis("Vertical")) * moveSpeed;
         transform.LookAt(transform.position + moveVector);
         rig.AddForce(moveVector);
-        pickup();
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,26 +50,77 @@ public class PlayerScript : MonoBehaviour
         {
             Debug.Log("You have opened the " + other.GetComponentInChildren<Transform>().transform.parent.name + "." + " Do you want to pick it up?");
             tempName = other.GetComponentInChildren<Transform>().transform.parent.name;
+            Debug.Log("Pick which hand you want to pick up the object with");
+        }
+
+        if (other.name == "Furnace")
+        {
+            Debug.Log("Furnace Call");
+        }
+
+        if (other.name == "Basket")
+        {
+            Debug.Log("basket Call");
+        }
+
+        if (other.name == "bucket/Trash")
+        {
+            Debug.Log("Trash Call");
+            lefthand.GetComponent<Button>().onClick.AddListener(() => drop());
+        }
+
+        if (other.name == "Cauldron")
+        {
+            Debug.Log("Cauldron Call");
+        }
+
+        if (other.name == "Cutting Board")
+        {
+            Debug.Log("Cutting Board Call");
         }
     }
 
     void pickup()
     {
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            LeftHand.Add(tempName);
-            Debug.Log("You have picked up that item");
-            tempName = "";
-            LeftHand.ForEach(Debug.Log);
-            Debug.Log(tempName + "string dude");
-        }
-       
+        
+    }
 
-        else if (Input.GetKeyDown(KeyCode.N))
+   public void LeftButton()
+    {
+        Debug.Log("Left Button");
+        Left.Add(tempName);
+        tempName = "";
+        Debug.Log(Left[0]);
+        if (Left.Count > 1)
         {
-            Debug.Log("You did not pick up that item");
+            Debug.Log("Hand is full");
+        }
+    }
+    public void RightButton()
+    {
+        Debug.Log("Right Button");
+        Right.Add(tempName);
+        tempName = "";
+        Debug.Log(Right[0]);
+        if (Right.Count > 1)
+        {
+            Debug.Log("Hand is full");
         }
     }
 
+    public void drop()
+    {
+        //if the left button has been clicked while someting is in the left hand list, drop that item and remove it from the list
+        if (Left.Count == 1)
+        {
+            Debug.Log("you can place item into ");
+        }
+    }
+
+
+    void kitchenInteractions()
+    {
+
+    }
 
 }
